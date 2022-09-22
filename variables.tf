8d9e17b8a6a3ef319d191098a9867279a1f04621
insummus <seyang731@gmail.com>
2022-09-22 22:15:39 +0900

    edit

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "prefix" {
 
 variable "region" {
   description = "The region where the resources are created."
-  default     = "us-east-2"
+  default     = "ap-northeast-2"
 }
 
 variable "address_space" {
