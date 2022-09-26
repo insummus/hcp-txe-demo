251ea2d891f8597d752db7a488b2efd54c7f66e7
Sungtae Yang <seyang731@gmail.com>
2022-09-26 09:24:40 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -3,7 +3,7 @@ output "instance_name" {
   value = aws_instance.tfdemo.id
 }
 
-/*
+
 output "instance_ip" {
   value = aws_eip.tfdemo.public_ip
 }
@@ -11,4 +11,3 @@ output "instance_ip" {
 output "instance_ip_service_url" {
   value = "http://${aws_eip.tfdemo.public_ip}"
 }
-*/
