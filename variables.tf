2fd119f9858b25db7a8399e8abc2b7caddd28fdd
Sungtae Yang <seyang731@gmail.com>
2022-09-26 09:42:04 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "prefix" {
 
 variable "region" {
   description = "The region where the resources are created."
-  default     = "ap-northeast-2b"
+  default     = "us-east-1"
 }
 
 variable "address_space" {
