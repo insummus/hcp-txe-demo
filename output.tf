874fde335e95ebd69e5feb5188e31243cc2069e8
insummus <seyang731@gmail.com>
2022-11-18 00:32:59 +0900

    test1

--- output.tf
+++ output.tf
@@ -3,6 +3,11 @@ output "instance_name" {
   value = aws_instance.tfdemo.id
 }
 
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.tfdemo.public_ip
+}
+
 /*
 output "instance_ip" {
   value = aws_eip.tfdemo.public_ip
